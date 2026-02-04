e5da61545bc8d31cba4a7860817612a0be68a3e1
gary_lin <gary_lin@cmoney.com.tw>
2026-02-04 11:41:24 +0800

    設置部屬文件

--- vite.config.js
+++ vite.config.js
@@ -7,27 +7,29 @@ import { readFileSync } from "fs";
 const pkg = JSON.parse(readFileSync("./package.json", "utf-8"));
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  base: "/Japanese_Word_Practice_Vue/",
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [vue()],
+    base: mode === "prod" ? "/" : "/Japanese_Word_Practice_Vue/",
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
     },
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        // 將第三方庫打包到單獨的 chunk 中
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            return "vendor";
-          }
+    build: {
+      rollupOptions: {
+        output: {
+          // 將第三方庫打包到單獨的 chunk 中
+          manualChunks(id) {
+            if (id.includes("node_modules")) {
+              return "vendor";
+            }
+          },
         },
       },
     },
-  },
-  define: {
-    __APP_VERSION__: JSON.stringify(pkg.version),
-  },
+    define: {
+      __APP_VERSION__: JSON.stringify(pkg.version),
+    },
+  };
 });
