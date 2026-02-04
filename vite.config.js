8d04ad27544e96aa187196be33428b168264c24c
gary_lin <gary_lin@cmoney.com.tw>
2026-02-04 11:42:48 +0800

    調整版號

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,9 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { fileURLToPath, URL } from "node:url";
 import { readFileSync } from "fs";
-// import pkg from './package.json';
 
 const pkg = JSON.parse(readFileSync("./package.json", "utf-8"));
 
-// https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   return {
     plugins: [vue()],
